c997611b12dd03275714de6c588c941aca96c3cd
park <park@park>
2017-03-27 16:51:20 +0900

    sql.Date util.Date

---
+++ javaProject/src/cherUpBaby/Date/Date.java
@@ -0,0 +1,14 @@
+package cherUpBaby.Date;
+
+public class Date {
+ 
+
+	public static void main(String[] args) {
+	   
+		java.util.Date udilDate = new java.util.Date(System.currentTimeMillis());
+		System.out.println(udilDate);
+		
+		java.sql.Date SqlDate = new java.sql.Date(System.currentTimeMillis());
+		System.out.println(SqlDate);
+	}
+}
