b4cd49a7b2454274ea9b39049c124d6f9246efdc
park <park@park>
2017-03-28 04:13:50 +0900

    .

--- javaProject/src/cherUpBaby/basic/Initial.java
+++ javaProject/src/cherUpBaby/basic/Initial.java
@@ -12,24 +12,24 @@ public static void main(String[] args) {
   * 
   * Variable is name of the memory to prepared to store the value
   * Prepared Memory is [Memory Type]=MT [Variable Name]=VN
-  * [MT]   [VN]					[Memory]    
+  * [MT]   [VN]					[Memory]
   *	char   charharVaiableName;  2byte 16bit  java operate through(UTF-16)
-  * String stringVaiableName;	
-  *	byte   byteVaiableName;		1byte 8bit	
+  * String stringVaiableName;
+  *	byte   byteVaiableName;		1byte 8bit
   * short  shortVaiableName;	2byte 16bit
-  *	int    intVaiableName;		4byte 32bit	
+  *	int    intVaiableName;		4byte 32bit
   * long   longVaiableName;     8byte 64bit
   * float  floatVaiableName;	4byte 32bit
   * double doubleVaiableName;   8byte 64bit
   * 
-  *   * [MT]   [VN]				[Initialization]   
-  *	char   charharVaiableName   'A'  
+  * [MT]   [VN]				[Initialization]
+  *	char   charharVaiableName   'A'
   * String stringVaiableName	"AA"
-  *	byte   byteVaiableName      1		 	
+  *	byte   byteVaiableName      1
   * short  shortVaiableName	    1
-  *	int    intVaiableName		1	
+  *	int    intVaiableName		1
   * long   longVaiableName      1
-  * float  floatVaiableName	    
+  * float  floatVaiableName
   * double doubleVaiableName    3.14;
   * 
   * Member Field Variable(static variable, class variable) is initialized automatically 
